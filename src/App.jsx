52a4293bcbb7bf279e8b4e42696f094c842911a1
Wei <wlin.li2403@gmail.com>
2024-07-27 03:33:14 -0400

    creating sign up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
 // import { Button, Container, Row, Col } from 'react-bootstrap';
 import './App.css';
+
 import Header from '../Components/Header';
+import Homepage from '../Pages/Homepage';
+import LandingPage from '../Pages/LandingPage';
+import { useRoutes } from 'react-router-dom';
+import Login from '../Pages/Login';
+import Register from '../Pages/Register';
 
 
 function App() {
+
+  let linkElements = useRoutes([
+    {path: "/", element: <LandingPage />}, //landing page
+    {path: "home_page/*", element: <Homepage />}, //user home page
+    {path: "login/", element: <Login />},
+    {path: "register/", element: <Register />},
+  ])
+
   return (
     <div>
       <Header />
+      {/* routers */}
+      {/* everything below this should be based on routes */}
+      {linkElements}
+      
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
