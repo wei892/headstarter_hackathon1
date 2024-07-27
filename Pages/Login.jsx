c379b78450f97b6a77111210505f118fef2f48ff
Wei <wlin.li2403@gmail.com>
2024-07-27 04:25:46 -0400

    added usestates to handle data

--- Pages/Login.jsx
+++ Pages/Login.jsx
@@ -1,8 +1,36 @@
+import { Form, Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Login = () =>{
     return(
         <div>
+            <div className="registerCont">
+                <h2> Log In </h2>
+                {/* name, age, gender, height, activity, inital weight, goal weight */}
+                <Form>
+                    <Form.Group as={Row} className="mb-3" controlId="formEmail">
+                        <Form.Label column sm={2}>Email</Form.Label>
+                        <Col sm={10}>
+                            <Form.Control type="email" placeholder="name@example.com" />
+                        </Col>
+                    </Form.Group>
 
+                    <Form.Group as={Row} className="mb-3" controlId="formEmail">
+                        <Form.Label column sm={2}>Password</Form.Label>
+                        <Col sm={10}>
+                            <Form.Control type="email" placeholder="password" />
+                        </Col>
+                    </Form.Group>
+                </Form>
+
+                <Link to="../">
+                    <Button> Create Account </Button>
+                </Link>
+                <br></br>
+                <Link to="">
+                    Forgot Password
+                </Link>
+            </div>
         </div>
     )
 }
--- Pages/Register.jsx
+++ Pages/Register.jsx
@@ -1,11 +1,57 @@
 
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import "./Styling/Register.css"
 
 import { Button, Col, Dropdown, DropdownButton, Form, Row } from "react-bootstrap";
+import { useState } from "react";
 
 
 const Register = () =>{
+
+    // const [email, setEmail] = useSearchParams('');
+    // const [username, setUsername] = useState('');
+    // const [password, setPassword] = useState('');
+    // const [gender, setGender] = useState('');
+    // const [initialWeight, setInitialWeight] = useState('');
+    // const [goalWeight, setGoalWeight] = useState('');
+    // const [activityLevel, setActivityLevel] = useState('');
+    // //gotta find the correct bootstrap design and add in correct data
+    // const [birthDate, setBirthDate] = useState('');
+    // const [birthMonth, setBirthMonth] = useState(0);
+    // const [birthDay, setBirthDay] = useState(0);
+    // const [birthYear, setBirthYear] = useState(0);
+
+
+    // const handleEmailChange = (e) => setEmail(e.target.value);
+    // const handleUsernameChange = (e) => setUsername(e.target.value);
+    // const handlePasswordChange = (e) => setPassword(e.target.value);
+    // const handleGenderSelect = (gender) => setGender(gender);
+    // const handleInitialWeightChange = (e) => setInitialWeight(e.target.value);
+    // const handleGoalWeightChange = (e) => setGoalWeight(e.target.value);
+    // const handleActivityLevelSelect = (level) => setActivityLevel(level);
+
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     const formData = {
+    //       email,
+    //       username,
+    //       gender,
+    //       initialWeight,
+    //       goalWeight,
+    //       activityLevel,
+    //       //add birthdate 
+    //     };
+    //     console.log(formData); // Process form data here (e.g., send to backend)
+    // };
+
+    //format weight and and set birthday value
+    // const formatBirthDate = () =>{
+    //     // enteredWeight 
+    // }
+
+    //need to do tmr: handle input changes and form submission
+    // fix date formating
+
     return(
         <div>
             <div className="registerCont">
@@ -16,6 +62,13 @@ const Register = () =>{
                         <Form.Label column sm={2}>Email</Form.Label>
                         <Col sm={10}>
                             <Form.Control type="email" placeholder="name@example.com" />
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} className="mb-3" controlId="formEmail">
+                        <Form.Label column sm={2}>Password</Form.Label>
+                        <Col sm={10}>
+                            <Form.Control type="email" placeholder="password" />
                         </Col>
                     </Form.Group>
 
@@ -56,36 +109,12 @@ const Register = () =>{
                             </DropdownButton>
                         </Col>
                         <Col sm={10}>
-                            <DropdownButton id="dropdown-basic-button" title="Date">
-                            <Dropdown.Item>Janurary</Dropdown.Item>
-                            <Dropdown.Item>February</Dropdown.Item>
-                            <Dropdown.Item>March</Dropdown.Item>
-                            <Dropdown.Item>April</Dropdown.Item>
-                            <Dropdown.Item>May</Dropdown.Item>
-                            <Dropdown.Item>June</Dropdown.Item>
-                            <Dropdown.Item>July</Dropdown.Item>
-                            <Dropdown.Item>August</Dropdown.Item>
-                            <Dropdown.Item>September</Dropdown.Item>
-                            <Dropdown.Item>October</Dropdown.Item>
-                            <Dropdown.Item>November</Dropdown.Item>
-                            <Dropdown.Item>December</Dropdown.Item>
+                            <DropdownButton id="dropdown-basic-button" title="Day">
                             </DropdownButton>
                         </Col>
 
                         <Col sm={10}>
                             <DropdownButton id="dropdown-basic-button" title="Year">
-                            <Dropdown.Item>Janurary</Dropdown.Item>
-                            <Dropdown.Item>February</Dropdown.Item>
-                            <Dropdown.Item>March</Dropdown.Item>
-                            <Dropdown.Item>April</Dropdown.Item>
-                            <Dropdown.Item>May</Dropdown.Item>
-                            <Dropdown.Item>June</Dropdown.Item>
-                            <Dropdown.Item>July</Dropdown.Item>
-                            <Dropdown.Item>August</Dropdown.Item>
-                            <Dropdown.Item>September</Dropdown.Item>
-                            <Dropdown.Item>October</Dropdown.Item>
-                            <Dropdown.Item>November</Dropdown.Item>
-                            <Dropdown.Item>December</Dropdown.Item>
                             </DropdownButton>
                         </Col>
                     </Form.Group>
@@ -115,7 +144,10 @@ const Register = () =>{
                             </DropdownButton>
                         </Col>
                     </Form.Group>
+                
+
                 </Form>
+
 
                 <Link to="../">
                     <Button> Create Account </Button>
